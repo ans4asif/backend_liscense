3f857688a75e541980106013aa421afa22420363
ans4asif <ans4asif@gmail.com>
2023-11-15 16:03:39 +0500

    udpate

--- puppeteer.config.cjs
+++ puppeteer.config.cjs
@@ -13,7 +13,7 @@ console.log({
 });
 
 console.log({ path_of_cache: join(__dirname, '.cache', 'puppeteer') });
-const puppeteerCachePath = '/home/ans4asif2/.cache/puppeteer';
+const puppeteerCachePath = '/home/ans4asif2/backend_liscense/.cache/puppeteer';
 module.exports = {
   // Changes the cache location for Puppeteer.
   // cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
